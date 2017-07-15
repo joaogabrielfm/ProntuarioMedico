fa25631052a63845ffb1cb5173a4202e0d4e2a69
victorferrari <victorferrari13@gmail.com>
2017-07-15 18:56:06 -0300

    Tabela plano de saude

---
+++ PM/Database/tbl_plano_saude.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tbl_plano_saude]
+(
+	[CPF] VARCHAR(14) NOT NULL , 
+    [codPlano] INT NOT NULL, 
+    [data_contratacao] DATETIME NOT NULL, 
+    [nome_operadora] VARCHAR(50) NOT NULL, 
+    [valor_atualizado] FLOAT NOT NULL, 
+    [carencia] DATE NOT NULL, 
+    [data_reajuste] DATE NOT NULL, 
+    [tipo_plano ] VARCHAR(20) NOT NULL, 
+    [valor_mensal] FLOAT NOT NULL, 
+    [nome varchar] VARCHAR(40) NOT NULL, 
+    CONSTRAINT [FK_tbl_plano_saude_pessoa_ToTable] FOREIGN KEY ([CPF]) REFERENCES [tbl_pessoa]([CPF]), 
+    PRIMARY KEY ([CPF],[codPlano])
+)
