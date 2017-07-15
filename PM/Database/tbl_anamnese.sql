b79e28201ae8c1c901652033a4238fab4aebb806
victorferrari <victorferrari13@gmail.com>
2017-07-15 17:52:23 -0300

    Criação das tabelas anamnese e exame

---
+++ PM/Database/tbl_anamnese.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbl_anamnese]
+(
+	[CPF] VARCHAR(14) NOT NULL , 
+    [Id_anamnese] INT NOT NULL, 
+    CONSTRAINT [FK_tbl_anamnese_pessoa_ToTable] FOREIGN KEY ([CPF]) REFERENCES [tbl_pessoa]([CPF]), 
+    CONSTRAINT [PK_tbl_anamnese] PRIMARY KEY ([CPF], [Id_anamnese]),
+)
---
+++ PM/Database/tbl_exame.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[tbl_exame]
+(
+	[data_hora] DATETIME NOT NULL PRIMARY KEY, 
+    [resultado] TEXT NULL
+)
