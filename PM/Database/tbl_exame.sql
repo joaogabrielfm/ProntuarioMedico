bfff128c3694fbd90d1a7d8ed51f2a83062e028d
Henrique Savelli <henrique.savelli@gmail.com>
2017-07-16 18:46:02 -0300

    Consertou o projeto.

--- PM/Database/tbl_exame.sql
+++ PM/Database/tbl_exame.sql
@@ -1,5 +1,5 @@
 ﻿CREATE TABLE [dbo].[tbl_exame]
 (
-	[Data_hora] DATETIME NOT NULL PRIMARY KEY, 
-    [Resultado] TEXT NOT NULL
+	[data_hora] DATETIME NOT NULL PRIMARY KEY, 
+    [resultado] TEXT NULL
 )
--- PM/Database/tbl_plano_saude.sql
+++ PM/Database/tbl_plano_saude.sql
@@ -1,15 +1,15 @@
 ﻿CREATE TABLE [dbo].[tbl_plano_saude]
 (
-    [CodPlano] INT NOT NULL, 
-    [Data_contratacao] DATETIME NOT NULL, 
-    [Nome_operadora] VARCHAR(50) NOT NULL, 
-    [Valor_atualizado] FLOAT NOT NULL, 
-    [Carencia] DATE NOT NULL, 
-    [Data_reajuste] DATE NOT NULL, 
-    [Tipo_plano] VARCHAR(20) NOT NULL, 
-    [Valor_mensal] FLOAT NOT NULL, 
-    [Nome] VARCHAR(40) NOT NULL, 
-	[CPF] VARCHAR(14) NOT NULL,
-	CONSTRAINT [FK_tbl_plano_saude_Pessoa_ToTable] FOREIGN KEY ([CPF]) REFERENCES [tbl_pessoa]([CPF]),
-    CONSTRAINT [PK_tbl_plano_saude] PRIMARY KEY ([CPF],[CodPlano]),
+	[CPF] VARCHAR(14) NOT NULL , 
+    [codPlano] INT NOT NULL, 
+    [data_contratacao] DATETIME NOT NULL, 
+    [nome_operadora] VARCHAR(50) NOT NULL, 
+    [valor_atualizado] FLOAT NOT NULL, 
+    [carencia] DATE NOT NULL, 
+    [data_reajuste] DATE NOT NULL, 
+    [tipo_plano ] VARCHAR(20) NOT NULL, 
+    [valor_mensal] FLOAT NOT NULL, 
+    [nome varchar] VARCHAR(40) NOT NULL, 
+    CONSTRAINT [FK_tbl_plano_saude_pessoa_ToTable] FOREIGN KEY ([CPF]) REFERENCES [tbl_pessoa]([CPF]), 
+    PRIMARY KEY ([CPF],[codPlano])
 )
