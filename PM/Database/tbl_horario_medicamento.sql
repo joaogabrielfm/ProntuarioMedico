767bf40f39831a823c1a4b15064d4bfb1b2708f6
Eduardo Noronha <e-d-u-22@hotmail.com>
2017-07-17 01:33:56 -0300

    Criação de tabela de horário de medicamento

---
+++ PM/Database/tbl_horario_medicamento.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbl_horario_atendimento]
+(
+	[Nro_registro] INT NOT NULL, 
+    [Horario] TIME NOT NULL, 
+    CONSTRAINT [FK_tbl_horario_Med_ToTable] FOREIGN KEY ([Nro_registro]) REFERENCES [tbl_medicamento]([Nro_registro]),
+	CONSTRAINT PK_horarioMedicamento PRIMARY KEY ([Horario], [Nro_registro])
+)
