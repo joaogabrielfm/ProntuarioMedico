be52b89224fc3554bce9077d4b8899ace6355279
Henrique Savelli <henrique.savelli@gmail.com>
2017-07-17 00:55:28 -0300

    Criação de views de classe para pessoa e estabelecimento

---
+++ PM/Database/v_classe_estabelecimento.sql
@@ -0,0 +1,6 @@
+﻿CREATE VIEW [dbo].[v_classe_estabelecimento]
+	AS SELECT CNPJ, 'H' as tipo FROM [tbl_hospital] 
+	UNION SELECT CNPJ, 'L' as tipo FROM [tbl_laboratorio] 
+	UNION SELECT CNPJ, 'C' as tipo FROM [tbl_clinica]
+	UNION SELECT CNPJ, 'F' as tipo FROM [tbl_email_farmacia]
+
