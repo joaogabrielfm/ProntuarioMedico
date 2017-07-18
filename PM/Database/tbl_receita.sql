e574e451e93de7c1c0bbdc5ed535cb21799bd2fd
Jhony Ramos <jhony.juarez@monitoratec.com.br>
2017-07-17 23:07:53 -0300

    Create tables tbl_receita and tbl_usada_na_compra

---
+++ PM/Database/tbl_receita.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[tbl_receita]
+(
+	[id_receita] int IDENTITY(1,1) NOT NULL,
+	[CPF_paciente] varchar(14) NOT NULL,
+	[CPF_medico] varchar(14) NOT NULL,
+	[data_hora] DATETIME NOT NULL,
+	[Descricao] varchar(255),
+	CONSTRAINT [FK_receita_atendimentoMedico] FOREIGN KEY ([CPF_paciente], [CPF_medico], [data_hora])
+	REFERENCES [dbo].[tbl_atendimentoMedico]([CPF_paciente], [CPF_medico], [data_hora]),
+	CONSTRAINT [PK_receita] PRIMARY KEY ([id_receita]),
+	CONSTRAINT [UC_receita] UNIQUE ([CPF_paciente], [CPF_medico], [data_hora])
+);
+
---
+++ PM/Database/tbl_usada_na_compra.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[tbl_usada_na_compra]
+(
+	[id_receita] int NOT NULL,
+	[Nro_registro] int NOT NULL,
+	[data] DATETIME NOT NULL,
+	CONSTRAINT [FK_usadaNaCompra_receita] FOREIGN KEY ([id_receita])
+	REFERENCES [dbo].[tbl_receita]([id_receita]),
+	CONSTRAINT [FK_usadaNaCompra_medicamento] FOREIGN KEY ([Nro_registro])
+	REFERENCES [dbo].[tbl_medicamento]([Nro_registro]),
+	CONSTRAINT PK_usadaNaCompra PRIMARY KEY ([id_receita],[Nro_registro])
+);
+
