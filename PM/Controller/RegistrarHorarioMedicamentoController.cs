2e922676f2a995a819251541d7e801d92b7b6632
EduardoNoronha22 <551520@comp.ufscar.br>
2017-07-17 23:45:36 -0300

    Criação Front End Medicamento

---
+++ PM/Controller/RegistrarHorarioMedicamentoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Model;
+using DatabaseAccess;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    class RegistrarHorarioMedicamentoController : _BaseController
+    {
+        public RegistrarHorarioMedicamentoController(string connectionString) : base(connectionString){ }
+
+        public bool registrarHorarioMedicamento()
+        {
+            HorarioMedicamentoAccess horario = new HorarioMedicamentoAccess(SqlConConnectionString);
+            return true;
+        }
+
+        public void adicionaHorarioMedicamento(HorarioMedicamento horario)
+        {
+            MedicamentoAccess newMedicamento = new MedicamentoAccess(SqlConConnectionString);
+            newMedicamento.InsertMedicamento(horario);
+        }
+    }
+}
---
+++ PM/PMFrontEnd/RegistrarMedicamento.aspx.cs
@@ -0,0 +1,51 @@
+﻿using Model;
+using Controller;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PMFrontEnd
+{
+    public partial class RegistrarMedicamento : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bt_Create_Click(object sender, EventArgs e)
+        {
+            RegistrarMedicamentoController controller = new RegistrarMedicamentoController(ConfigurationManager.ConnectionStrings["DBInformation"].ToString());
+            if (tb_Nome.Text != null && tb_Numero_Registro.Text != null && tb_Posologia.Text != null && tb_Principio_Ativo.Text != null)
+            {
+                Medicamento medicamento = novoMedicamento();
+                //if (!controller.verificaMedicamento(medicamento))
+                //{
+                    controller.adicionaMedicamento(medicamento);
+                    Response.Redirect("default.aspx", false);
+                    ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "Cadastro realizado com sucesso.", "alert('Cadastro realizado com sucesso.')", true);
+                //}
+                //else {
+                //    Lb_Alert.Text = "Medicamento já cadastrado";
+                //}
+            }
+            else
+                Lb_Alert.Text = "Por favor, preencha todos os campos.";
+        }
+
+        public Medicamento novoMedicamento()
+        {
+            Medicamento medicamento = new Medicamento();
+            medicamento.nome = tb_Nome.Text;
+            medicamento.nro_registro = tb_Numero_Registro.Text;
+            medicamento.posologia = tb_Posologia.Text;
+            medicamento.principio_ativo = tb_Principio_Ativo.Text;
+           
+            return medicamento;
+        }
+    }
+}
