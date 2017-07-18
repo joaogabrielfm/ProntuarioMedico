0a7ec398fc9ed44b6e2eafdd40d648fba1885efd
Jhony Ramos <jhony.juarez@monitoratec.com.br>
2017-07-17 21:48:59 -0300

    tbl_imagem_exame.sql

--- PM/Database/tbl_imagem_exame.sql
+++ PM/Database/tbl_imagem_exame.sql
@@ -3,7 +3,7 @@
 	[data_hora] datetime NOT NULL,  
 	[imagem] image,
 	CONSTRAINT FK_imagemExame_Exame FOREIGN KEY (data_hora) 
-	REFERENCES [dbo].[tbl_exame](data_hora), 
+	REFERENCES [dbo].[tbl_exame](DataHora), 
 	CONSTRAINT PK_imagemExame PRIMARY KEY (data_hora, imagem)
 
 )
