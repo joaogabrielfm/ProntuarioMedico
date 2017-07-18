b63fdcb2e4e12b150c6138e405095e0401678126
joaogabrielfm <joaogabriel.fmarins@gmail.com>
2017-07-18 00:49:22 -0300

    removendo erro por conflito das fk

--- PM/Database/tbl_atualiza_anamnese.sql
+++ PM/Database/tbl_atualiza_anamnese.sql
@@ -5,5 +5,5 @@
     [CPF_medico] VARCHAR(14) NOT NULL, 
     CONSTRAINT [FK_atualizaAnamnese_anamnese] FOREIGN KEY ([CPF], [Id_anamnese]) REFERENCES [tbl_anamnese]([CPF], [Id_anamnese]),
     CONSTRAINT [FK_atualizaAnamnese_medico] FOREIGN KEY ([CPF_medico]) REFERENCES [tbl_medico]([CPF]),
-    CONSTRAINT [PK_anamnese] PRIMARY KEY ([CPF], [Id_anamnese], [CPF_medico])
+    CONSTRAINT [PK_atualiza_anamnese] PRIMARY KEY ([CPF], [Id_anamnese], [CPF_medico])
 )
